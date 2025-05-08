b7ae42586cc5af5808576d6b2a4a5cd7532a15ed
Allen Thomas <allenthom05@gmail.com>
2025-05-08 00:40:17 -0400

    set up Mediapipe drawings

--- Detection.py
+++ Detection.py
@@ -20,16 +20,24 @@ def mediapipe_detection(image, model):
     image.flags.writeable = True 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) 
     return image, results
-
+def draw_landmarks(image, results):
+    #draw the left hand
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                               mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+                               mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2))
+    #draw the right hand
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                               mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+                               mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2))
 cap = cv2.VideoCapture(0)
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
         ret, frame = cap.read()
         image, results = mediapipe_detection(frame, holistic)
-        print(results)
+        draw_landmarks(image, results)
         cv2.imshow('frame', image)
-        
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 cap.release()
+print(results.right_hand_landmarks)
 cv2.destroyAllWindows()
